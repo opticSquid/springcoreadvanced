5ad9a6a93e15dfee688ba99a65a39c84b44a818a
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-04-26 20:38:19 +0530

    Auto wiring using annotation setter injection

--- src/main/java/com/spring/core/springcoreadvanced/autowire/Employee.java
+++ src/main/java/com/spring/core/springcoreadvanced/autowire/Employee.java
@@ -1,13 +1,8 @@
 package com.spring.core.springcoreadvanced.autowire;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Employee {
-	// while auto wiring by name the dependency variable name must match the bean
-	// name in this case it is address so in config file there has to be a bean
-	// named "address"
-	/*
-	 * To be Noted In this ByName auto-wiring the bean name should be in small case
-	 * and hence the variable name should also be in small case
-	 */
 	private Adrs address;
 
 	Employee(Adrs address) {
@@ -17,7 +12,7 @@ public class Employee {
 	public Adrs getAddress() {
 		return address;
 	}
-
+	@Autowired
 	public void setAddress(Adrs Address) {
 		this.address = Address;
 	}
