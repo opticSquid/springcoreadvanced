8a782ad1602271e5182af5039abb04ae5adb8392
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-04-26 20:30:36 +0530

    auto wiring using byName

--- src/main/java/com/spring/core/springcoreadvanced/autowire/Employee.java
+++ src/main/java/com/spring/core/springcoreadvanced/autowire/Employee.java
@@ -1,14 +1,21 @@
 package com.spring.core.springcoreadvanced.autowire;
 
 public class Employee {
+	// while auto wiring by name the dependency variable name must match the bean
+	// name in this case it is address so in config file there has to be a bean
+	// named "address"
+	/*
+	 * To be Noted In this ByName auto-wiring the bean name should be in small case
+	 * and hence the variable name should also be in small case
+	 */
 	private Adrs address;
 
 	public Adrs getAddress() {
 		return address;
 	}
 
-	public void setAddress(Adrs address) {
-		this.address = address;
+	public void setAddress(Adrs Address) {
+		this.address = Address;
 	}
 
 	@Override
