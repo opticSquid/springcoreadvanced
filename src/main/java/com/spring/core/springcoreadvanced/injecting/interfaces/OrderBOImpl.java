dbf513b87596e38090265af4f9b442ed61d7ff80
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-04-27 15:49:17 +0530

    Using dependency injection of interfaces using annotations

--- src/main/java/com/spring/core/springcoreadvanced/injecting/interfaces/OrderBOImpl.java
+++ src/main/java/com/spring/core/springcoreadvanced/injecting/interfaces/OrderBOImpl.java
@@ -1,7 +1,12 @@
 package com.spring.core.springcoreadvanced.injecting.interfaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("bo")
 public class OrderBOImpl implements OrderBO {
 
+	@Autowired
 	private OrderDAO dao;
 	@Override
 	public void placeOrder() {
--- src/main/java/com/spring/core/springcoreadvanced/injecting/interfaces/OrderDAOImpl.java
+++ src/main/java/com/spring/core/springcoreadvanced/injecting/interfaces/OrderDAOImpl.java
@@ -1,5 +1,8 @@
 package com.spring.core.springcoreadvanced.injecting.interfaces;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderDAOImpl implements OrderDAO {
 
 	@Override
